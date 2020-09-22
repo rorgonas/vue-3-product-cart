c83f908af41f6f5e4abb013d230522ce9607ed2b
Rolland Orgonas <rolland.orgonas@mavtek.com>
2020-09-22 13:16:06 -0400

    class - add - style binding

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ const app = Vue.createApp({
             product: 'Socks',
             brand: 'Vue Mastery',
             image: './assets/images/socks_blue.jpg',
-            inStock: false,
+            inStock: true,
             details: ['50% cotton', '30% wool', '20% polyester'],
             variants: [
               { id: 2234, color: 'green', image: './assets/images/socks_green.jpg' },
@@ -15,7 +15,11 @@ const app = Vue.createApp({
     },
     methods: {
         addToCart() {
-            this.cart += 1
+            if (this.cart >= 5) {
+                this.inStock = false;
+                return;
+            }
+            this.cart += 1;
         },
         updateImage(variantImage) {
             this.image = variantImage
